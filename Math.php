ddabaf4eda5208c4dc444f7f138c464f7365aae7
codisart <louis.celier@gmail.com>
2015-09-24 18:03:27 +0200

    [STYLE] Suppression code mort commenté.

--- Math.php
+++ Math.php
@@ -13,24 +13,26 @@ class Math {
 	 * permet d'utiliser la fonction min de php sans renvoyer les valeurs false (NULL, FALSE, "")
 	 * @return mixed la valeur du paramètre considéré comme "inférieure" suivant la comparaison standard
 	 */
-	public function min() {		
-		// var_dump(func_get_args());
-    	$variables = array_filter(func_get_args(), 'strlen');    	
-    	return min($variables);
+	public function min() {
+		$variables = array_filter(func_get_args(), 'strlen');
+		return min($variables);
 	}
 
 	/**
 	 * permet d'utiliser la fonction min de php sans renvoyer les valeurs false (NULL, FALSE, "")
 	 * @return mixed la valeur du paramètre considéré comme "inférieure" suivant la comparaison standard
 	 */
-	public function max() {		
-    	$variables = array_filter(func_get_args(), 'strlen');
-    	return max($variables);
+	public function max() {
+		$variables = array_filter(func_get_args(), 'strlen');
+		return max($variables);
 	}
 
+	/**
+	 *
+	 */
 	public function isBetween($int,$first,$second){
 		$min = min($first,$second);
 		$max = max($first,$second);
-	    return ($min<$int && $int<$max);
+		return ($min<$int && $int<$max);
 	}
 }
