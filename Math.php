8154f95634553af1a77cfbc340ba70f6463cda41
codisart <louis.celier@gmail.com>
2015-10-13 20:48:21 +0200

    [TECH] Ajoute les calculs de déterminants et de produit scalaire.

--- Math.php
+++ Math.php
@@ -13,7 +13,7 @@ class Math {
 	 * permet d'utiliser la fonction min de php sans renvoyer les valeurs false (NULL, FALSE, "")
 	 * @return mixed la valeur du paramètre considéré comme "inférieure" suivant la comparaison standard
 	 */
-	public function min() {
+	public static function min() {
 		$variables = array_filter(func_get_args(), 'strlen');
 		return min($variables);
 	}
@@ -22,7 +22,7 @@ class Math {
 	 * permet d'utiliser la fonction min de php sans renvoyer les valeurs false (NULL, FALSE, "")
 	 * @return mixed la valeur du paramètre considéré comme "inférieure" suivant la comparaison standard
 	 */
-	public function max() {
+	public static function max() {
 		$variables = array_filter(func_get_args(), 'strlen');
 		return max($variables);
 	}
@@ -30,9 +30,39 @@ class Math {
 	/**
 	 *
 	 */
-	public function isBetween($int, $first, $second) {
+	public static function isBetween($int, $first, $second) {
 		$min = min($first, $second);
 		$max = max($first, $second);
 		return ($min < $int && $int < $max);
+	}
+
+	/**
+	 * [determinant description]
+	 * @param  Segment $vertexOne    [description]
+	 * @param  Segment $vertexSecond [description]
+	 * @return integer               [description]
+	 */
+	public static function determinant(Segment $vertexOne, Segment $vertexSecond) {
+		$abscissaVertexOne = $vertexOne->getPointB()->getAbscisse() - $vertexOne->getPointA()->getAbscisse();
+		$ordinateVertexOne = $vertexOne->getPointB()->getOrdonnee() - $vertexOne->getPointA()->getOrdonnee();
+		$abscissaVertexSecond = $vertexSecond->getPointB()->getAbscisse() - $vertexSecond->getPointA()->getAbscisse();
+		$ordinateVertexSecond = $vertexSecond->getPointB()->getOrdonnee() - $vertexSecond->getPointA()->getOrdonnee();
+
+		return ($abscissaVertexOne * $ordinateVertexSecond) - ($abscissaVertexSecond * $ordinateVertexOne);
+	}
+
+	/**
+	 * [scalarProduct description]
+	 * @param  Segment $vertexOne    [description]
+	 * @param  Segment $vertexSecond [description]
+	 * @return integer                [description]
+	 */
+	public static function scalarProduct (Segment $vertexOne, Segment $vertexSecond) {
+		$abscissaVertexOne = $vertexOne->getPointB()->getAbscisse() - $vertexOne->getPointA()->getAbscisse();
+		$ordinateVertexOne = $vertexOne->getPointB()->getOrdonnee() - $vertexOne->getPointA()->getOrdonnee();
+		$abscissaVertexSecond = $vertexSecond->getPointB()->getAbscisse() - $vertexSecond->getPointA()->getAbscisse();
+		$ordinateVertexSecond = $vertexSecond->getPointB()->getOrdonnee() - $vertexSecond->getPointA()->getOrdonnee();
+
+		return ($abscissaVertexOne * $abscissaVertexSecond) + ($ordinateVertexOne * $ordinateVertexSecond);
 	}
 }
